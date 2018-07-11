bfe899a0c1003f1f84ca14cd83e92266b0ffc9f1
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-11 11:54:37 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,10 +4,10 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     guess = gets.chomp
     computer_guess = rand 1..6
-    if guess != computer_guess
+    if guess.to_i != computer_guess
       puts "The computer guessed #{computer_guess}."
       break
-    elsif guess == computer_guess
+    elsif guess.to_i == computer_guess
       puts "You guessed the correct number!"
       break
     
